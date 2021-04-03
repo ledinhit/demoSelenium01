9b1cf69dbe1be1e4e2c1b09f21b029880e028537
ledinhit <30380091+ledinhit@users.noreply.github.com>
2021-04-03 11:32:16 +0700

    fix Unable to locate element button
    fix Unable to locate element button

--- src/selenium/AddNewPostTest.java
+++ src/selenium/AddNewPostTest.java
@@ -1,6 +1,10 @@
 package selenium;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class AddNewPostTest extends BaseTest {
@@ -9,9 +13,9 @@ public class AddNewPostTest extends BaseTest {
 	String user_login = "user_login";
 	String user_pass = "user_pass";
 	String submitBtn = "wp-submit";
-	String title = "editor-post-title__input";
-	String content = "block-editor-rich-text__editable";
-	String publishBtn ="components-button";
+	String title = "//textarea[@id='post-title-0']";
+	String content = "//div[@class = 'block-editor-writing-flow']/div/p";
+	String publishBtn ="//button[contains(text(),'Publish')]";
 
 	public void login(String user, String pass) {
 		driver.findElement(By.id(user_login)).sendKeys(user);
@@ -23,9 +27,16 @@ public class AddNewPostTest extends BaseTest {
 	public void TC_01_AddNewPostTest() {
 		login("dinhlt", "121212");
 		driver.get(URL_addNewPost);
-		driver.findElement(By.className(title)).sendKeys("Title");
-		driver.findElement(By.className(content)).sendKeys("Content");
-		driver.findElement(By.className(publishBtn)).click();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath(title)).sendKeys("This is the Title");
+//		driver.switchTo().frame("block-editor-block-list__layout is-root-container");
+		WebElement body = driver.findElement(By.xpath(content));
+//		driver.switchTo().defaultContent();
+		WebElement publishBtn1 = driver.findElement(By.xpath(publishBtn));
+		Actions action =new Actions(driver);
+		action.moveToElement(body).click().sendKeys(body,"This is the body").build();
+//		action.moveToElement(publishBtn1).click().build().perform();
+		
 	}
   
 }
